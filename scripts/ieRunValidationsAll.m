d733ee74460ee499621704ba48bb687173854d53
Brian Wandell <wandell@stanford.edu>
2023-07-19 12:09:09 -0700

    Update ieRunValidationsAll.m
    Integrating validation testing across ISET related repositories.

--- scripts/ieRunValidationsAll.m
+++ scripts/ieRunValidationsAll.m
@@ -1,28 +1,48 @@
-function ieRunValidationsAll
+function ieRunValidationsAll(repo)
 %ieRunTutorialsAll
 %
 % Description:
-%   Run all of the isetbio validations that we think should work, and print out a report at the end
-%   as to whether they threw errors, or not.  This does not check the validation data, it just
-%   runs through the scripts.
-
-% 10/19/17  dhb  Wrote this from tutorials script
-
-% User/project specific preferences
-p = struct(...
-    'rootDirectory',            fileparts(which(mfilename())), ...
-    'tutorialsSourceDir',       fullfile(isetValidatePath, 'isetbio') ...                % local directory where tutorial scripts are located
-    );
-
-%% List of scripts to be skipped from automatic publishing.
+%   Run validations and print out a report at the end.  Implemented
+%   originally for ISETBio, and then extended to other repositories
+%   through the isetvalidate repository method.
 %
-% Anything with this in its path name is skipped.
-scriptsToSkip = {...
-    'codedevscripts' ...
-    'xNeedChecking' ...
-    'rgc' ...
-    };
+%   Requires the Unit test repository on your path.
+
+if notDefined('repo'), repo = 'isetcam'; end
+
+switch ieParamFormat(repo)
+    case 'isetcam'
+        p = struct(...
+            'rootDirectory',           isetValidatePath, ...
+            'tutorialsSourceDir',      fullfile(isetValidatePath, 'isetcam') ...
+            );
+
+        scriptsToSkip = {...
+            'v_ISET' ...
+            'v_manualViewer',...
+            'data'  ...          
+            };
+
+        fprintf('*** We suggest running v_manualViewer from time to time.***\n');
+
+    case 'isetbio'
+        % User/project specific preferences
+        p = struct(...
+            'rootDirectory',            fileparts(which(mfilename())), ...
+            'tutorialsSourceDir',       fullfile(isetValidatePath, 'isetbio') ...
+            );
+
+        % Anything with this in its path name is skipped.
+        scriptsToSkip = {...
+            'codedevscripts', ...
+            'xNeedChecking', ...
+            'rgc' ...
+            };
+    otherwise
+        error('Unknown repo %s\n',repo);
+end
 
 %% Use UnitTestToolbox method to do this.
 UnitTest.runProjectTutorials(p, scriptsToSkip, 'All');
+
 end
