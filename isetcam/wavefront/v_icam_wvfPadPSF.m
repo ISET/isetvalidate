7970b8a9bdcb86b364d3f4eaef95d84549e4b29b
Brian Wandell <1837145+wandell@users.noreply.github.com>
2024-04-15 18:51:08 -0700

    Update v_icam_wvfPadPSF.m

--- isetcam/wavefront/v_icam_wvfPadPSF.m
+++ isetcam/wavefront/v_icam_wvfPadPSF.m
@@ -4,12 +4,12 @@
 % ImageConvFrequencyDomain issue.
 %
 
-% This scene produces the error.  
+ieInit;
+
+%% This scene produces the error.  
 oi = oiCreate('human wvf');
 
-%%
 scene = sceneCreate('slanted edge');
-
 
 oi = oiCompute(oi,scene,'pad value','mean','crop',false);
 sz = oiGet(oi,'size');
@@ -17,27 +17,6 @@ sz = oiGet(oi,'size');
 % Notice that the line rolls off to zero and does not stay at the mean
 % level.
 oiPlot(oi,'illuminance hline',round([1 sz(2)/2]));
-
-%% This scene is OK.
-scene = sceneCreate('macbeth d65');
-oi = oiCompute(oi,scene,'pad value','mean','crop',false);
-sz = oiGet(oi,'size');
-
-% There is no roll off to zero with this scene.
-oiPlot(oi,'illuminance hline',round([1 sz(2)/2]));
-
-%% Here are other scenes I tried.  They were OK, too.
-
-%{
-scene = sceneCreate('grid lines');
-scene = sceneCreate('line d65');
-scene = sceneCreate('point array');
-
-oi = oiCompute(oi,scene,'pad value','mean','crop',false);
-sz = oiGet(oi,'size');
-oiPlot(oi,'illuminance hline',round([1 sz(2)/2]));
-
-%}
 
 %% Compare the opticsOTF  path.  It does not have the error.
 
@@ -53,18 +32,64 @@ sz = oiGet(oi,'size');
 % level.
 oiPlot(oi,'illuminance hline',round([1 sz(2)/2]));
 
-%% But opticsPSF does
+%% OpticsPSF with pad value of zero
 
 oi = oiCreate('human wvf');
 
 scene = sceneCreate('slanted edge');
 oi = oiSet(oi,'optics name','opticspsf');
 
-oi = oiCompute(oi,scene,'pad value','mean','crop',false);
+oi = oiCompute(oi,scene,'pad value','zero','crop',false);
 sz = oiGet(oi,'size');
 
 % Notice that the line rolls off to zero and does not stay at the mean
 % level.
+uDataPSF = oiPlot(oi,'illuminance hline',round([1 sz(2)/2]));
+
+%
+oi = oiCreate('human wvf');
+
+scene = sceneCreate('slanted edge');
+oi = oiSet(oi,'optics name','opticsotf');
+
+oi = oiCompute(oi,scene,'pad value','zero','crop',false);
+sz = oiGet(oi,'size');
+
+% 
+% There is a shift of one pixel between the two methods
+%
+% Notice that the line rolls off to zero and does not stay at the mean
+% level.
+uDataOTF = oiPlot(oi,'illuminance hline',round([1 sz(2)/2]));
+
+dx = uDataOTF.pos(2) - uDataOTF.pos(1);
+ieNewGraphWin;
+plot(uDataOTF.pos - dx,uDataOTF.data,'ro',uDataPSF.pos,uDataPSF.data,'g-');
+grid on;
+
+
+%% Here are other scenes I tried.  They were OK, too.
+
+%{
+
+scene = sceneCreate('grid lines',211);
+scene = sceneCreate('line d65',127);
+scene = sceneCreate('point array');
+scene = sceneCreate('macbeth d65');
+scene = sceneCreate('slantedEdge',128,1.33,[], [], 0.9); 
+
+oi = oiCreate('human wvf');
+oi = oiSet(oi,'optics name','opticspsf');
+oi = oiCompute(oi,scene,'pad value','mean','crop',false);
+sz = oiGet(oi,'size');
 oiPlot(oi,'illuminance hline',round([1 sz(2)/2]));
 
-%%
+
+oi = oiCreate('human wvf');
+oi = oiSet(oi,'optics name','opticsotf');
+oi = oiCompute(oi,scene,'pad value','mean','crop',false);
+sz = oiGet(oi,'size');
+oiPlot(oi,'illuminance hline',round([1 sz(2)/2]));
+
+%}
+
