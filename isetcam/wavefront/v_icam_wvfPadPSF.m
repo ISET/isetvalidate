f5a57126b9859f8e0ed505df22d88246e582f498
Brian Wandell <1837145+wandell@users.noreply.github.com>
2024-04-16 12:29:53 -0700

    Update v_icam_wvfPadPSF.m

--- isetcam/wavefront/v_icam_wvfPadPSF.m
+++ isetcam/wavefront/v_icam_wvfPadPSF.m
@@ -4,25 +4,39 @@
 % ImageConvFrequencyDomain issue.
 %
 
+%%  Set up the scene
 ieInit;
 
-%% This scene produces the error.  
-oi = oiCreate('human wvf');
+% When the size is even, no shift.  When the size is odd, there is a
+% one pixel shift.  See this by comparing the line d65 cases.
 
-scene = sceneCreate('slanted edge');
+% But for this scene the size is always constructed odd, even when we
+% specify even!  Check why that is.
+% scene = sceneCreate('slanted edge');
+
+% The agreement is better as imSize is smaller
+% imSize = 512; scene = sceneCreate('grid lines',imSize,imSize/4);
+% scene = sceneCreate('macbeth d65');
+scene = sceneCreate('line d65',255);  % Shift
+% scene = sceneCreate('line d65',256);  % No shift
+
+scene = sceneSet(scene,'fov',1);
+
+%% This scene produced the error when using the old code
+
+oi = oiCreate('human wvf');
 
 oi = oiCompute(oi,scene,'pad value','mean','crop',false);
 sz = oiGet(oi,'size');
 
-% Notice that the line rolls off to zero and does not stay at the mean
-% level.
+% With the old code, the plot rolls off to zero and does not stay at
+% the mean level.
 oiPlot(oi,'illuminance hline',round([1 sz(2)/2]));
 
 %% Compare the opticsOTF  path.  It does not have the error.
 
 oi = oiCreate('human wvf');
 
-scene = sceneCreate('slanted edge');
 oi = oiSet(oi,'optics name','opticsotf');
 
 oi = oiCompute(oi,scene,'pad value','mean','crop',false);
@@ -36,7 +50,6 @@ oiPlot(oi,'illuminance hline',round([1 sz(2)/2]));
 
 oi = oiCreate('human wvf');
 
-scene = sceneCreate('slanted edge');
 oi = oiSet(oi,'optics name','opticspsf');
 
 oi = oiCompute(oi,scene,'pad value','zero','crop',false);
@@ -46,50 +59,18 @@ sz = oiGet(oi,'size');
 % level.
 uDataPSF = oiPlot(oi,'illuminance hline',round([1 sz(2)/2]));
 
-%
 oi = oiCreate('human wvf');
-
-scene = sceneCreate('slanted edge');
 oi = oiSet(oi,'optics name','opticsotf');
-
 oi = oiCompute(oi,scene,'pad value','zero','crop',false);
 sz = oiGet(oi,'size');
-
-% 
-% There is a shift of one pixel between the two methods
-%
-% Notice that the line rolls off to zero and does not stay at the mean
-% level.
 uDataOTF = oiPlot(oi,'illuminance hline',round([1 sz(2)/2]));
 
-dx = uDataOTF.pos(2) - uDataOTF.pos(1);
+%% When size of photons is odd, there is a shift of 1 pixel.
+
 ieNewGraphWin;
-plot(uDataOTF.pos - dx,uDataOTF.data,'ro',uDataPSF.pos,uDataPSF.data,'g-');
-grid on;
+plot(uDataOTF.pos,uDataOTF.data,'ro',uDataPSF.pos,uDataPSF.data,'gs');
+grid on;  title('No shift'); legend({'OTF','PSF'});
 
 
-%% Here are other scenes I tried.  They were OK, too.
-
-%{
-
-scene = sceneCreate('grid lines',211);
-scene = sceneCreate('line d65',127);
-scene = sceneCreate('point array');
-scene = sceneCreate('macbeth d65');
-scene = sceneCreate('slantedEdge',128,1.33,[], [], 0.9); 
-
-oi = oiCreate('human wvf');
-oi = oiSet(oi,'optics name','opticspsf');
-oi = oiCompute(oi,scene,'pad value','mean','crop',false);
-sz = oiGet(oi,'size');
-oiPlot(oi,'illuminance hline',round([1 sz(2)/2]));
-
-
-oi = oiCreate('human wvf');
-oi = oiSet(oi,'optics name','opticsotf');
-oi = oiCompute(oi,scene,'pad value','mean','crop',false);
-sz = oiGet(oi,'size');
-oiPlot(oi,'illuminance hline',round([1 sz(2)/2]));
-
-%}
+%% END
 
