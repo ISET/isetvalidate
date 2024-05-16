b4798e3d9869a04db623fd3aa86ffca60230cb7d
Brian Wandell <1837145+wandell@users.noreply.github.com>
2024-05-16 15:38:09 -0700

    Additional tests and fixed one test.

---
+++ isetcam/sensor/v_icam_sensorIMX490.m
@@ -0,0 +1,37 @@
+%% Testing the IMX490 sensor compute algorithm
+%
+% I had trouble with the IMX363 sensor and the autoexposure
+% aeLuminance.  See comments in autoExposure and v_icam_sensorimx363.
+
+%%
+ieInit;
+
+%%
+scene = sceneCreate('macbeth d65');
+oi = oiCreate; oi = oiCompute(oi,scene);
+
+%%
+sensor = sensorCreate('imx490-large');
+sensor = sensorSet(sensor,'noise flag',0);
+
+sensor = sensorSet(sensor,'fov',sceneGet(scene,'fov'),oi);
+sensor = sensorCompute(sensor,oi);
+% sensorWindow(sensor);
+
+%% The mean volts should be the same.
+volts = sensorGet(sensor,'volts');
+assert(abs( (mean(volts,'all')/0.2316522) - 1) < 1e-5);
+
+%%
+sensor = sensorCreate('imx490-small');
+sensor = sensorSet(sensor,'noise flag',0);
+
+sensor = sensorSet(sensor,'fov',sceneGet(scene,'fov'),oi);
+sensor = sensorCompute(sensor,oi);
+% sensorWindow(sensor);
+
+%% The mean volts should be the same.
+volts = sensorGet(sensor,'volts');
+assert(abs( (mean(volts,'all')/0.2280384) - 1) < 1e-5);
+
+%% End
