1abd1c40102ff48ded4fe90fcb3425120ff9378b
David Cardinal <github@cardinalphoto.com>
2024-09-12 16:13:17 -0700

    initial scripts for hdrsensor

---
+++ isethdrsensor/v_ihdrsensor_livescripts.m
@@ -0,0 +1,26 @@
+% Validate that all the livescripts used to generate 
+% the figures for isethdrsensor are working
+%
+%
+%% Currently just a list
+
+s_hsSkyBrightness.mlx
+% Tested & fixed, Runs, 9/12/24, D.Cardinal on Windows
+
+s_hsFig_LightGroups.mlx
+% Runs, 9/12/24, D.Cardinal on Windows
+
+s_hsFigSensorflare.mlx
+% Runs, 9/12/24, D.Cardinal on Windows
+
+s_hsFigExposure.mlx
+% Runs, 9/12/24, D.Cardinal on Windows
+
+s_hsFigSensorTunnelDay.mlx
+% Runs, 9/12/24, D.Cardinal on Windows
+
+s_hsFigRGBW_Evaluation
+% Problem with python code for nn!
+
+
+
