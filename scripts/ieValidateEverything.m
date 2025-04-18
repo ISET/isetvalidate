2d84f25a352adb42a92b90dbaa5afe53a7b4d8e5
David Brainard <brainard@psych.upenn.edu>
2025-04-18 12:47:16 -0400

    Run many things, although not everything

--- scripts/ieValidateEverything.m
+++ scripts/ieValidateEverything.m
@@ -1,6 +1,6 @@
 % ieValidateEverything
 %
-% Run all our validation and regression scripts, and save output of each to
+% Runmany ofl our validation and regression scripts, and save output of each to
 % a file.
 
 %% Examples
@@ -9,6 +9,14 @@ ieExamples('isetbio');
 ieExamples('csfgenerator');
 
 %% Validations
-ieValidate(isetcam','scripts');
-ieValidate(isetcam,'tutorials');
-ieValidate(isetcam,'validations');
+ieValidate('isetcam','scripts');
+ieValidate('isetcam,'tutorials');
+ieValidate('isetcam','validations');
+
+ieValidate('isetbio','scripts');
+ieValidate('isetbio','tutorials');
+ieValidate('isetbio','validations');
+
+ieValidate('csfgenerator','scripts');
+ieValidate('csfgenerator','tutorials');
+ieValidate('csfgenerator','validations');
