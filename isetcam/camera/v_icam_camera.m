e03f72852e50b1720e81830fbb47fb0b877058b0
Brian Wandell <1837145+wandell@users.noreply.github.com>
2025-05-28 16:03:24 -0700

    Adding comments from Gemini
    It's kind of wordy.

--- isetcam/camera/v_icam_camera.m
+++ isetcam/camera/v_icam_camera.m
@@ -62,53 +62,5 @@ assert(cameraGet(c,'sensor noise flag') == noiseFlag, 'Camera sensor noise flag 
 % Verify the pixel size of the sensor within the camera.
 assert(isequal(cameraGet(c,'pixel size','um'),[1.5 1.5]), 'Camera pixel size does not match [1.5 1.5] um.');
 
-% The following line is commented out as it opens a UI window, which
-% might not be desired during automated validation. Uncomment if manual
-% visual inspection is needed.
-% cameraWindow(c);
-
-%{
-%% v_camera
-%
-% Tests creation of the camera structure, making sure that it uses the
-% oi and sensor that are currently in the database by default.
-
 %%
-ieInit
 
-%% Test that 'ideal' cameraCreate works
-c = cameraCreate('ideal');
-
-assert(cameraGet(c,'sensor noise flag') == -1);
-assert(cameraGet(c,'sensor nfilters') == 1);
-
-fprintf('Ideal noise flag %d. Nfilters = %d\n',...
-    cameraGet(c,'sensor noise flag'), ...
-    cameraGet(c,'sensor nfilters'));
-
-%% Test cameraCreate for 'current'
-oi     = oiCreate;
-optics = oiGet(oi,'optics');
-optics = opticsSet(optics,'fnumber',22);
-oi     = oiSet(oi,'optics',optics);
-ieAddObject(oi);
-
-%%  Sensor added to environment
-
-sensor = sensorCreate('human');
-noiseFlag = sensorGet(sensor,'noise flag');
-ieAddObject(sensor);
-
-%% These should be the names of the objects built aboves
-c = cameraCreate('current');
-
-assert(strncmp(cameraGet(c,'sensor name'),'human',5));
-assert(cameraGet(c,'optics fnumber')  == 22);   % 22
-assert(cameraGet(c,'sensor noise flag') == noiseFlag); % 2
-assert(isequal(cameraGet(c,'pixel size','um'),[1.5 1.5]));
-
-% Nothing to see
-% cameraWindow(c);
-
-%%
-%}
